3a20e00d5ccb3f544aa00052861832efc69ebfad
codeplay13 <muzic.maniac7@gmail.com>
2020-07-26 00:25:41 +0530

    Update CVOTE.cpp

--- CVOTE.cpp
+++ CVOTE.cpp
@@ -6,34 +6,35 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	int n, m, max=-1;
-	string s1, s2, name, country;
+	int n, m;
+	string name, country;
 	unordered_map<string, string> name_country;
 	unordered_map<string, int> country_count, name_count;
 	cin>>n>>m;
 	while(n--){
-	    cin>>s1>>s2;
-	    name_country.insert({s1, s2});
+	    cin>>name>>country;
+	    name_country.insert({name, country});
 	}
 	while(m--){
-	    cin>>s1;
-	    name_count[s1]++;
-	    country_count[name_country[s1]]++;
+	    cin>>name;
+	    name_count[name]++;
+	    country_count[name_country[name]]++;
 	}
+	m = -1;
 	for(auto i: country_count){
-	    if(i.second>max){
-	        max = i.second;
+	    if(i.second>m){
+	        m = i.second;
 	        country = i.first;
 	    }
-	    else if(i.second==max) country = min(country, i.first);
+	    else if(i.second==m) country = min(country, i.first);
 	}
-	max = -1;
+	m = -1;
 	for(auto i: name_count){
-	    if(i.second>max){
-	        max = i.second;
+	    if(i.second>m){
+	        m = i.second;
 	        name = i.first;
 	    }
-	    else if(i.second==max) name = min(name, i.first);
+	    else if(i.second==m) name = min(name, i.first);
 	}
 	cout<<country<<"\n"<<name;
 	return 0;
